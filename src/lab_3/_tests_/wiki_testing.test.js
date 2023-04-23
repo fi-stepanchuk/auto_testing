32d5ad59da1b472c0aba27f8c8a75d1736ecbb3f
Andrii Stepanchuk <20fi.a.stepanchuk@std.npu.edu.ua>
2023-04-23 22:44:44 +0300

    add to test timeout

--- src/lab_3/_tests_/wiki_testing.test.js
+++ src/lab_3/_tests_/wiki_testing.test.js
@@ -10,6 +10,6 @@ describe('Google Testing', () => {
         expect(testingResults).toHaveProperty('exists');
         expect(testingResults).toHaveProperty(['exists', 'emblem'], true);
         expect(testingResults.numberArthiMonumentsInList).toBeGreaterThan(20);
-    });
+    }, 60000);
 });
 
